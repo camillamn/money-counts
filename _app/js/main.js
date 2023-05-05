d19eea796319267477fdf2666636c64d1ea7e287
Camilla <112381378+camillamn@users.noreply.github.com>
2023-05-05 14:01:25 +0200

    Added weekly-list to main.js

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,5 +1,7 @@
 import UserList from './modules/user-list.js';
-import TaskList from './modules/task-list.js'
+import TaskList from './modules/task-list.js';
+import WeeklyList from './modules/weekly-list.js';
 
 UserList();
 TaskList();
+WeeklyList();
