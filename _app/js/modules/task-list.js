97cb2e1c8c37816bbf916dba8712c6d1b5c6ab50
camillamn <112381378+camillamn@users.noreply.github.com>
2023-05-05 14:17:26 +0200

    Minor typo fixes (#60)
    * Removed space in function
    * Removed spacing
    * Added comment to weekly-list

--- _app/js/modules/task-list.js
+++ _app/js/modules/task-list.js
@@ -1,13 +1,12 @@
 import { sanity } from '../sanity.js';
 
-export default async function TaskList () {
+export default async function TaskList() {
    const query = `*[_type == 'task'] | order(name asc) {
       name,
       value
    }`;
 
    // fetch tasks from the sanity database using query
-
    const tasks = await sanity.fetch(query);
 
    console.log(tasks)
