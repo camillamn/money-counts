e0e478c311ef8854ea589be12709569b76924a19
Camilla <112381378+camillamn@users.noreply.github.com>
2023-05-10 09:04:01 +0200

    Added user to WeeklyList

--- _app/js/modules/weekly-list.js
+++ _app/js/modules/weekly-list.js
@@ -1,15 +1,15 @@
 import { sanity } from '../sanity.js';
 
-export default async function WeeklyList() {
+export default async function WeeklyList(user) {
 	const query = `*[_type == 'weeklyList'] | order(weekNumber asc){
 		weekNumber,
 		"user": user->{username},
 		 "userAvatar": user->avatar.asset->url,
 		 tasks[]{
 			date,
-			"task": task->{
-			  name,
-			  value
+			 "task": task->{
+				 name,
+			  	value
 			}
 		 }
 	  }`;
@@ -17,5 +17,39 @@ export default async function WeeklyList() {
 	// fetch weekly-lists sorted by weekNumber from sanity
 	const weeklyPerformedTasksLists = await sanity.fetch(query);
 
-	// console.log(weeklyPerformedTasksLists)
-}
+	console.log(weeklyPerformedTasksLists)
+
+	const weeklyListContainer = document.createElement('div');
+
+	for (const weeklyPerformedTasksList of weeklyPerformedTasksLists) {
+
+		const weekNumber = document.createElement('h3');
+		weekNumber.textContent = `Week ${weeklyPerformedTasksList.weekNumber}`;
+
+		const tasksList = document.createElement('ul');
+
+		for (const task of weeklyPerformedTasksList.tasks) {
+			
+			const taskListItem = document.createElement('li');
+			
+			const taskDate = document.createElement('span');
+			taskDate.textContent = task.date;
+			
+			const taskName = document.createElement('span');
+			taskName.textContent = task.task.name;
+			
+			const taskValue = document.createElement('span');
+			taskValue.textContent = task.task.value;
+			
+			taskListItem.appendChild(taskDate);
+			taskListItem.appendChild(taskName);
+			taskListItem.appendChild(taskValue);
+			tasksList.appendChild(taskListItem);
+		}
+			weeklyListContainer.appendChild(weekNumber);
+			weeklyListContainer.appendChild(tasksList);
+		}
+
+	 return weeklyListContainer;
+  }
+
