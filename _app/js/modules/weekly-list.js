2c5e705ff18993fccc5078a044f11b78aef095ed
camillamn <112381378+camillamn@users.noreply.github.com>
2023-05-11 12:38:10 +0200

    Style static user page (#76)
    * Changed classname on section
    * Created static-page-users and added styling
    * Imported the css file to main
    * Refactored the code due to the styling

--- _app/js/modules/weekly-list.js
+++ _app/js/modules/weekly-list.js
@@ -1,57 +1,79 @@
-// import { sanity } from '../sanity.js';
+/** 
+ * @TODO - need to fetch userAlt as well
+*/
+
 import FetchWeeklyList from './fetch-weekly-lists.js';
 
 export default async function WeeklyList() {
 
-	const weeklyListOfPerformedTasks = await FetchWeeklyList('user');
-const weeklyListContainer = document.querySelector('.weekly-list__container');
+	const weeklyListOfPerformedTasks = await FetchWeeklyList();
+	const weeklyListContainer = document.querySelector('.static-page-users');
+
+	let currentUser = '';
+	let weekList = null;
 
 	for (const weeklyListOfPerformedTask of weeklyListOfPerformedTasks) {
+		if (currentUser !== weeklyListOfPerformedTask.user.username) {
+			currentUser = weeklyListOfPerformedTask.user.username;
 
-		const weekNumber = document.createElement('h3');
+			const userInfo = document.createElement('div');
+			const userImage = document.createElement('figure');
+			const userImg = document.createElement('img');
+			const userName = document.createElement('div');
+			
+			weekList = document.createElement('ul');
+
+			userInfo.className = 'static-page-users__user-information grid__column--4 box';
+			userImage.className = 'static-page-users__user-image';
+			userImg.className = 'static-page-users__user-img';
+			
+			userName.className = 'static-page-users__user-name box';
+			weekList.className = 'static-page-users__week-list box';
+
+			userName.innerText = weeklyListOfPerformedTask.user.username;
+			userImg.src = weeklyListOfPerformedTask.userAvatar;
+			// userImg.alt = weeklyListOfPerformedTask.username;
+
+			userImage.appendChild(userImg);
+			userInfo.appendChild(userImg);
+			userInfo.appendChild(userName);
+			userInfo.appendChild(weekList);
+			weeklyListContainer.appendChild(userInfo);
+		}
+
+		const weekNumber = document.createElement('li');
+		const tasksList = document.createElement('ul');
+
+		weekNumber.className = 'static-page-users__week-number box';
+		tasksList.className = 'static-page-users__tasks-list';
+
 		weekNumber.textContent = `Week ${weeklyListOfPerformedTask.weekNumber}`;
 
-		const userAvatar = weeklyListOfPerformedTask.userAvatar;
-		const userName = weeklyListOfPerformedTask.user.username;
-		
-		const userInfo = document.createElement('div');
-		userInfo.classList.add('user-info');
-
-		const userAvatarImg = document.createElement('img');
-		userAvatarImg.src = userAvatar;
-
-		const userNameSpan = document.createElement('span');
-		userNameSpan.textContent = `User: ${userName}`;
-
-		userInfo.appendChild(userAvatarImg);
-		userInfo.appendChild(userNameSpan);
-		weeklyListContainer.appendChild(userInfo);
-
-
-		const tasksList = document.createElement('ul');
+		weekList.appendChild(weekNumber);
 
 		for (const task of weeklyListOfPerformedTask.tasks) {
 			
 			const taskListItem = document.createElement('li');
+			const taskDate = document.createElement('div');
+			const taskName = document.createElement('div');
+			const taskValue = document.createElement('div');
 			
-			const taskDate = document.createElement('span');
-			taskDate.textContent = task.date;
+			taskListItem.className = 'static-page-users__tasks-list-item grid';
+			taskDate.className = 'static-page-users__task-date grid__column--4';
+			taskName.className = 'static-page-users__task-name grid__column--6';
+			taskValue.className = 'static-page-users__task-value grid__column--2';
 			
-			const taskName = document.createElement('span');
 			taskName.textContent = task.task.name;
-			
-			const taskValue = document.createElement('span');
+			taskDate.textContent = task.date;
 			taskValue.textContent = task.task.value;
-			
+
+			tasksList.appendChild(taskListItem);
 			taskListItem.appendChild(taskDate);
 			taskListItem.appendChild(taskName);
 			taskListItem.appendChild(taskValue);
-			tasksList.appendChild(taskListItem);
 		}
-			weeklyListContainer.appendChild(weekNumber);
-			weeklyListContainer.appendChild(tasksList);
-		}
-
-	 return weeklyListContainer;
-  }
-
+		
+		weekNumber.appendChild(tasksList);
+	}
+	return weeklyListContainer;
+}
