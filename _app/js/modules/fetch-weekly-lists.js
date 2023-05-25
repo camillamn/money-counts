b1ee8be00fd3dacd37da41f21522c78f5973feb6
Camilla <112381378+camillamn@users.noreply.github.com>
2023-05-25 11:55:45 +0200

    Fixed typo and added _id and paid to fetch

--- _app/js/modules/fetch-weekly-lists.js
+++ _app/js/modules/fetch-weekly-lists.js
@@ -1,24 +1,26 @@
 import { sanity } from '../sanity.js';
 
-export default async function FetchWeeklyList() {
+export default async function FetchWeeklyLists() {
 	const query = `*[_type == 'weeklyList'] | order(weekNumber asc, tasks.date asc) {
+		_id,
 		weekNumber,
+		paid,
 		"user": user->{username},
 		"slug": user->slug.current,
 		"userAvatar": user->avatar.asset->url,
 		 tasks[]{
 			date,
 			 "task": task->{
-				 name,
+				name,
 			  	value
 			}
 		 } | order(date asc)
 	  }`;
 
 	// fetch weekly-lists sorted by weekNumber from sanity
-	const weeklyListOfPerformedTasks = await sanity.fetch(query);
+	const weeklyListsOfPerformedTasks = await sanity.fetch(query);
 
 	// console.log(weeklyListOfPerformedTasks)
 
-	return weeklyListOfPerformedTasks;
+	return weeklyListsOfPerformedTasks;
 }
