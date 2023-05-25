3df3f533aa1a35d640dcf8a28ff69ddc9c571cf8
Camilla <112381378+camillamn@users.noreply.github.com>
2023-05-25 22:11:19 +0200

    Imported function to user-page

--- _app/js/user-page.js
+++ _app/js/user-page.js
@@ -1,5 +1,7 @@
 import WeeklyList from "./modules/weekly-list.js";
 import TaskList from "./modules/task-list.js";
+import AddTasksToWeeklyList from "./modules/add-tasks-to-weekly-list.js";
 
 await WeeklyList();
 await TaskList();
+await AddTasksToWeeklyList();
