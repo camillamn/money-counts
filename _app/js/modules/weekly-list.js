be50e7d4d7ca3f4f4db5b6e4cdd9a847a06c91de
Camilla <112381378+camillamn@users.noreply.github.com>
2023-05-25 11:57:10 +0200

    Refactored and moved functions to separate module

--- _app/js/modules/weekly-list.js
+++ _app/js/modules/weekly-list.js
@@ -1,114 +1,59 @@
 /** 
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flatMap
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/Set
  * 
  * @TODO - need to fetch userAlt as well
  * 		- add errormessage if the slug is not to be found
  * 		- create the user no matter if there is a weeklyList of performed tasks or not
 */
 
-import FetchWeeklyList from './fetch-weekly-lists.js';
+import FetchWeeklyLists from './fetch-weekly-lists.js';
+import UserList from './user-list.js';
+import { createUserInfo } from './create-user-info.js';
 import { togglePerformedTasksList } from './toggle-performed-tasks-list.js';
 import { sumEarnings } from './calculate-earnings.js';
 import { readSlug } from '../util/read-slug.js';
+import { createDropdownMenu } from './create-dropdown-menu.js';
 import formatDate from '../util/format-date.js';
+import { getWeekNumber } from '../../../_studio/schemas/utils.js';
+import createWeeklyListOfPerformedTasks from './create-weekly-list-of-performed-tasks.js';
 
-export default async function WeeklyList() {
+export default async function WeeklyLists() {
+	const weeklyListsContainer = document.querySelector('.static-page-kids');
+	
+	const weeklyListsOfPerformedTasks = await FetchWeeklyLists();
 	const slug = readSlug();
-	const weeklyListOfPerformedTasks = await FetchWeeklyList();
+	console.log(slug);
 
 	let currentUser = null;
-	let weekList = null;
-	let totalEarnings = null;
+	const weeklyInfo = document.createElement('section');
+	weeklyInfo.className = 'static-page-kids__weekly-information grid__column--6 box';
 
-	const filteredWeeklyList = weeklyListOfPerformedTasks.filter(
-		task => task.user.username === slug);
+	const filteredWeeklyListsBySlug = weeklyListsOfPerformedTasks.filter(
+		weeklyList => weeklyList.user.username === slug
+	)
+	console.log(filteredWeeklyListsBySlug);
 
-		if (!filteredWeeklyList.length) {
-		return null;
-	}
+	for (const weeklyList of filteredWeeklyListsBySlug) {
+		// prevent duplicate of user and add the weekly lists to the current user based on slug
+		if (currentUser !== weeklyList.user.username) {
+			currentUser = weeklyList.user.username;
 
-	const weeklyListContainer = document.querySelector('.static-page-kids');
-
-
-	for (const weeklyListOfPerformedTask of filteredWeeklyList) {
-		// check if its the same user or not and either adds another task to the current user, or updates the current user
-		if (currentUser !== weeklyListOfPerformedTask.user.username) {
-			currentUser = weeklyListOfPerformedTask.user.username;
-
-			const userInfo = document.createElement('div');
-			const userImage = document.createElement('figure');
-			const userImg = document.createElement('img');
-			const userName = document.createElement('div');
-			
-			weekList = document.createElement('ul');
-
-			userInfo.className = 'static-page-kids__user-information grid__column--6 box';
-			userImage.className = 'static-page-kids__user-image';
-			userImg.className = 'static-page-kids__user-img';
-			
-			userName.className = 'static-page-kids__user-name grid__column--12 box';
-			weekList.className = 'static-page-kids__week-list grid';
-
-			userName.innerText = weeklyListOfPerformedTask.user.username;
-			userImg.src = weeklyListOfPerformedTask.userAvatar;
-			// userImg.alt = weeklyListOfPerformedTask.username;
-
-			userImage.appendChild(userImg);
-			userInfo.appendChild(userImg);
-			userInfo.appendChild(userName);
-			userInfo.appendChild(weekList);
-			weeklyListContainer.appendChild(userInfo);
+		const userInfo = createUserInfo(
+			weeklyList.user.username, 
+			weeklyList.userAvatar
+		);
+			weeklyListsContainer.appendChild(userInfo);
 		}
-
-		const weekNumber = document.createElement('button');
-		const performedTasksList = document.createElement('ul');
-		const earningsItem = document.createElement('div');
-
-		const earnings = sumEarnings(weeklyListOfPerformedTask.tasks);
-		totalEarnings += earnings;
-
-		weekNumber.className = 'static-page-kids__week-number grid__column--12 box';
-		performedTasksList.className = 'static-page-kids__performed-tasks-list box';
-		earningsItem.className = 'static-page-kids__earnings';
 		
-		weekNumber.textContent = `Uke ${weeklyListOfPerformedTask.weekNumber} --- Du har tjent ${earnings.toString()} kroner`;
-		// earningsItem.textContent = `Du har tjent ${earnings.toString()} kroner denne uken`;
-		
-		weekList.appendChild(weekNumber);
+		const earnings = sumEarnings(weeklyList.tasks);
 
-		weekNumber.addEventListener('click', () => {
-			togglePerformedTasksList(performedTasksList);
-		});
-		
-		for (const task of weeklyListOfPerformedTask.tasks) {
-			const taskListItem = document.createElement('li');
-			const taskDate = document.createElement('div');
-			const taskName = document.createElement('div');
-			const taskValue = document.createElement('div');
-			
-			taskListItem.className = 'static-page-kids__performed-tasks-list-item grid';
-			taskDate.className = 'static-page-kids__task-date grid__column--3 grid__column-mobile--3';
-			taskName.className = 'static-page-kids__task-name grid__column--8 grid__column-mobile--8';
-			taskValue.className = 'static-page-kids__task-value grid__column-1 grid__column-mobile--1';
-			
-			taskName.textContent = task.task.name;
-			taskDate.textContent = formatDate(task.date);
-			taskValue.textContent = task.task.value;
-
-			performedTasksList.appendChild(taskListItem);
-			taskListItem.appendChild(taskDate);
-			taskListItem.appendChild(taskName);
-			taskListItem.appendChild(taskValue);
+		const performedTasksList = createWeeklyListOfPerformedTasks(
+			weeklyList, 
+			earnings
+		);
+		weeklyInfo.appendChild(performedTasksList);
 		}
-		weekNumber.appendChild(performedTasksList);
-		weekNumber.appendChild(earningsItem);
-	}
-
-	// total earnings
-	const totalEarningsItem = document.createElement('div');
-	totalEarningsItem.className = 'static-page-kids__total-earnings grid__column--12 box';
-	totalEarningsItem.textContent = `Totalt har du tjent ${totalEarnings} kroner i år`;
-	weeklyListContainer.appendChild(totalEarningsItem);
-
-	return weeklyListContainer;
+	weeklyListsContainer.appendChild(weeklyInfo);
 }
