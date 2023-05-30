1cc0fc70ba7e2aac26d096b23bb9ea4dfccccf76
Camilla <112381378+camillamn@users.noreply.github.com>
2023-05-30 23:03:29 +0200

    Refactored code to add toggle

--- _app/js/modules/toggle-performed-tasks-list.js
+++ _app/js/modules/toggle-performed-tasks-list.js
@@ -1,7 +1,3 @@
 export function togglePerformedTasksList(performedTasksList) {
-	if (performedTasksList.classList.contains('static-page-kids__performed-tasks-list--visible')) {
-		performedTasksList.classList.remove('static-page-kids__performed-tasks-list--visible');
-	} else {
-		performedTasksList.classList.add('static-page-kids__performed-tasks-list--visible');
-	}
+	performedTasksList.classList.toggle('dynamic-page-kids__performed-tasks-list--visible');
 }
--- _app/js/modules/toggle-task-buttons.js
+++ _app/js/modules/toggle-task-buttons.js
@@ -1,7 +1,3 @@
 export function toggleTaskButtons(taskButtonsList) {
-	if (taskButtonsList.classList.contains('static-page-kids__task-buttons-list--visible')) {
-		taskButtonsList.classList.remove('static-page-kids__task-buttons-list--visible');
-	} else {
-		taskButtonsList.classList.add('static-page-kids__task-buttons-list--visible');
-	}
+taskButtonsList.classList.toggle('dynamic-page-kids__task-buttons-list--visible');
 }
