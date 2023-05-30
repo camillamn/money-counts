faebbde8f99fff45cd665ed5570d8e51dc487ac4
Camilla <112381378+camillamn@users.noreply.github.com>
2023-05-30 22:53:30 +0200

    Changed format date to fit date in sanity schema

--- _app/js/util/format-date.js
+++ _app/js/util/format-date.js
@@ -2,13 +2,24 @@
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
  */
 
-export default function formatDate(date) {
+export function formatDateToFrontend(date) {
 	const options = {
 		// weekday: 'long',
-		day: 'numeric',
+		day: '2-digit',
 		month: 'long',
 		year: 'numeric',
 	}
-	const formattedDate = new Date(date).toLocaleDateString('no-NO', options);
-	return formattedDate;
+	const formattedDateToFrontend = new Date(date).toLocaleDateString('no-NO', options);
+	return formattedDateToFrontend;
+}
+
+export function formatDateToSanity(date) {
+	const options = {
+		day: '2-digit',
+		month: '2-digit',
+		year: 'numeric',
+	}
+	const formattedDateToSanity = new Date(date).toLocaleDateString('nl-NL', options);
+	return formattedDateToSanity;
+	
 }
