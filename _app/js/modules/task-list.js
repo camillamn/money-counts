518b95c35ef40efa3c9a3502937d31d590c56ed3
Camilla <112381378+camillamn@users.noreply.github.com>
2023-05-30 22:55:47 +0200

    Imported addTask function to the module

--- _app/js/modules/task-list.js
+++ _app/js/modules/task-list.js
@@ -1,6 +1,7 @@
 /**
  * https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset
  */
+import addTaskToWeeklyList from "./add-task-to-weekly-list.js";
 import FetchTasks from "./fetch-tasks.js";
 import { toggleTaskButtons } from "./toggle-task-buttons.js";
 
@@ -9,12 +10,14 @@ export default async function TaskList() {
 	console.log(tasks);
 
 	function createTaskItemDOM() {
-		const taskButtonsContainer = document.querySelector('.static-page-kids__task-buttons-container');
+		const taskButtonsContainer = document.querySelector('.dynamic-page-kids__task-buttons-container');
 		const openTaskButtonsList = document.createElement('button');
 		const taskButtonsList = document.createElement('div');
 
-		openTaskButtonsList.className = 'static-page-kids__open-task-buttons-list';
-		taskButtonsList.className = 'static-page-kids__task-buttons-list';
+		openTaskButtonsList.classList.add(
+			'dynamic-page-kids__open-task-buttons-list',
+			'grid');
+		taskButtonsList.classList.add('dynamic-page-kids__task-buttons-list');
 
 		openTaskButtonsList.addEventListener('click', () => {
 			toggleTaskButtons(taskButtonsList);
@@ -23,15 +26,31 @@ export default async function TaskList() {
 		for (const task of tasks) {
 			const taskButton = document.createElement('button');
 
-			taskButton.className = 'static-page-kids__task-button box';
+			taskButton.classList.add(
+				'dynamic-page-kids__task-button',
+				'grid__column--3', 
+				'box');
 			taskButton.innerText = `${task.name} ${task.value} kroner`;
 			taskButton.dataset.taskId = task._id;
+
+			taskButton.addEventListener('click', async (event) => {
+				try {
+					const clickedButton = event.target;
+					const taskId = clickedButton.dataset.taskId;
+					console.log(`Button with taskId: ${taskId} clicked`);
+					
+					const response = await addTaskToWeeklyList(taskId);
+					console.log(response);
+				} catch (error) {
+					console.error('An error occurred while sending the post request');
+				}
+			});
 
 			taskButtonsList.appendChild(taskButton);
 		}
 
 		openTaskButtonsList.innerText = 'Legg til oppgave';
-		openTaskButtonsList.className = 'static-page-kids__tasks-add-task';
+		openTaskButtonsList.className = 'dynamic-page-kids__tasks-add-task';
 
 		taskButtonsContainer.appendChild(openTaskButtonsList);
 		taskButtonsContainer.appendChild(taskButtonsList);
