8e321d6f2ebb017d46000d668e5877e47503dbb9
camillamn <112381378+camillamn@users.noreply.github.com>
2023-06-01 22:06:25 +0200

    Fix typos (#134)
    * Added h1 to header
    * Minor typo fixes
    * Changed header from div to h1

--- _app/js/modules/user-list.js
+++ _app/js/modules/user-list.js
@@ -18,71 +18,71 @@ export default async function UserList() {
 	// create the DOM elements
 	function createUserItemDOM(user) {
 
-			const userContent = document.createElement('article');
-			const userImage = document.createElement('figure');
-			const userImg = document.createElement('img');
-			// const userName = document.createElement('h3');
-			const userLink = document.createElement('a');
+		const userContent = document.createElement('article');
+		const userImage = document.createElement('figure');
+		const userImg = document.createElement('img');
+		// const userName = document.createElement('h3');
+		const userLink = document.createElement('a');
 
-			// add CSS classes to the elements
-			userContent.classList.add(
-				'frontpage-users__user-content',
-				'grid__column--4',
-				'box',
-			)
-			userImage.classList.add(
-				'frontpage-users__user-image',
-			);
-			userImg.classList.add(
-				'frontpage-users__user-img'
-			);
-			// userName.classList.add(
-			// 	'frontpage-users__user-name',
-			// 	'box',
-			// );
-			userLink.classList.add(
-				'frontpage-users__user-choose-me',
-				'box',
-			);
+		// add CSS classes to the elements
+		userContent.classList.add(
+			'frontpage-users__user-content',
+			'grid__column--4',
+			'box',
+		)
+		userImage.classList.add(
+			'frontpage-users__user-image',
+		);
+		userImg.classList.add(
+			'frontpage-users__user-img'
+		);
+		// userName.classList.add(
+		// 	'frontpage-users__user-name',
+		// 	'box',
+		// );
+		userLink.classList.add(
+			'frontpage-users__user-choose-me',
+			'box',
+		);
+		
+		// set the text content and source of the elements
+		userImg.src = user.userAvatar;
+		userImg.alt = user.userAlt;
+
+		// userName.innerText = user.username
+
+		userLink.setAttribute('href', `/kids/?${user.slug.current}`);
+		userLink.textContent = `Til ${user.username} sin side`;
+
+		// append the child elements to the parent element
+		userImage.appendChild(userImg);
+		userContent.appendChild(userImage);
+		// userContent.appendChild(userName);
+		userContent.appendChild(userLink);
+
+		// handle click event to go to the user page based on slug
+		userContent.addEventListener('click', (event) => {
+			event.preventDefault();
+			window.location.href = userLink.href;
+		});
 			
-			// set the text content and source of the elements
-			userImg.src = user.userAvatar;
-			userImg.alt = user.userAlt;
-
-			// userName.innerText = user.username
-
-			userLink.setAttribute('href', `/kids/?${user.slug.current}`);
-			userLink.textContent = `Til ${user.username} sin side`;
-
-			// append the child elements to the parent element
-			userImage.appendChild(userImg);
-			userContent.appendChild(userImage);
-			// userContent.appendChild(userName);
-			userContent.appendChild(userLink);
-
-			// handle click event to go to the user page based on slug
-			userContent.addEventListener('click', (event) => {
-				event.preventDefault();
-				window.location.href = userLink.href;
-			});
-			
-			return userContent;
-		}
-		// renders the user list by creating the DOM elements and append them to the document body
-		function renderHTML() {
-			const userListContainer = document.querySelector('.frontpage-users');
-			const userItem = document.createElement('section');
-			userItem.classList.add(
-				'frontpage-users__user',
-				'grid',
-			);
-
-			for (const user of users) {
-				const userContainer = createUserItemDOM(user);
-				userItem.appendChild(userContainer);
-			}
-			userListContainer.appendChild(userItem)
-			document.body.appendChild(userListContainer);
-		}
-		renderHTML();
+		return userContent;
 	}
+	// renders the user list by creating the DOM elements and append them to the document body
+	function renderHTML() {
+		const userListContainer = document.querySelector('.frontpage-users');
+		const userItem = document.createElement('section');
+		userItem.classList.add(
+			'frontpage-users__user',
+			'grid',
+		);
+
+		for (const user of users) {
+			const userContainer = createUserItemDOM(user);
+			userItem.appendChild(userContainer);
+		}
+		userListContainer.appendChild(userItem);
+		document.body.appendChild(userListContainer);
+	}
+	renderHTML();
+}
